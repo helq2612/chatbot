ec702571fddfa344e06ba88014b452f69cb43160
helq2612 <heli@oregonstate.edu>
2017-08-22 00:39:38 -0700

    add some chatbot functionalities

--- index.js
+++ index.js
@@ -21,12 +21,46 @@ app.get('/', function(req, res) {
 
 // Facebook
 
+let token = "EAAbNx8o9hx8BAEIO453RXdg2U069fqNc1vwSdqAMmw5sf1tQbFqpjrLOVd9R7NcIehsZCBsT3mADmcnAcCvdwoVZBVRzRyEd7O0ZCmGSG69B7ZC8MSG6rt9KZAcDAFICQt2X9jXVilnBQHEl930FU4GIQLHCPxSQI43osAp8Y5M8fxIRzgD4I"
 app.get('/webhook', function(req, res) {
 	if(req.query['hub.verify_token'] == 'helq2612'){
 		res.send(req.query['hub.challenge'])
 	}
 	res.send("Wrong token")
 })
+
+app.post('/webhook', function(req,res) {
+	let messaging_events = req.body.entry[0].messaging
+	for( let i = 0; i < messaging_events.length; i++){
+		let event = messaging_events[i];
+		let sender = event.sender.id
+		if(event.message && event.message.text){
+			let text = event.message.text
+			sendText(sender, "Text echo: " + text.substring(0,100))
+		}
+	}
+	res.sendStatus(200)
+
+})
+
+function sennText(sender, text){
+	let messageData = {text:text}
+	request({
+		url: "https://graph.facebook.com/v2.6/me/message"
+		qs : {access_token: token}
+		method: "POST"
+		json: {
+			receipient: {id : sender},
+			message : messageData
+		}
+	}, function(error, response, body){
+		if(error){
+			console.log("sending error!")
+		}else if(response.body.error){
+			console.log("response body error!")
+		}
+	})
+}
 
 app.listen(app.get('port'), function(){
 	console.log("running: port")
